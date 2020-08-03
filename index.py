f8a335a020cb0f247d86bf7d40d8a22fb8d4b561
Shinarthas <tttaaabbb@gmail.com>
2020-08-04 01:07:03 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,8 @@
+import  decision_tree_classification
+import  k_nearest_neighbors
+import kernel_svm
+import  logistic_regression
+import naive_bayes
+import random_forest_classification
+import  support_vector_machine
+
